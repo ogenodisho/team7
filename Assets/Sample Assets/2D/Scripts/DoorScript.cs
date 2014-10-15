96b73a2b87e79b6c667f8abdeadf354d054409ea
Tom <cqin645@aucklanduni.ac.nz>
2014-10-15 21:57:40 +1300

    Added door into the tutorial level, but not finished the end of level

---
+++ Assets/Sample Assets/2D/Scripts/DoorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+	Animator anim;
+	bool open;
+	
+	void Awake () {
+		open = false;
+		anim = GetComponent<Animator> ();
+		GetComponent<Collider2D> ().isTrigger = false;
+	}
+
+	void FixedUpdate () {
+
+	}
+
+	public void OpenDoor () {
+		anim.SetBool ("Open", true);
+		GetComponent<Collider2D> ().isTrigger = true;
+	}
+}
